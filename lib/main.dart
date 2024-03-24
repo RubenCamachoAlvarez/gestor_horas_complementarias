7e9e59b34f0dd4e54e1bfeeec036953c183bfafe
RubenCA <ruben1000.ca@gmail.com>
2024-03-24 00:46:44 -0600

    Adicion de firebase a la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:gestor_de_horas_complementarias/App.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
