e052d75303f19b0b5bdd0038981c4c079effe4fc
RubenCA <ruben1000.ca@gmail.com>
2024-03-23 21:05:58 -0600

    Diseño del primer login

--- lib/vistas/Login.dart
+++ lib/vistas/Login.dart
@@ -15,33 +15,142 @@ class LoginWidgetState extends State<LoginWidget> {
 
   LoginWidgetState();
 
+  Container contenedorPrincipal = Container(color: Colors.orange,
+
+    child: Column(
+
+      mainAxisAlignment: MainAxisAlignment.center,
+
+
+
+      children: [
+
+
+
+      ],
+
+    ),
+
+  );
+
   @override
 
   Widget build(BuildContext context) {
 
     return Scaffold(
 
-      appBar: AppBar(
+      body:
 
-        title: const Center(
+        Container(
 
-          child: Text("Gestor de horas complementarias",
+          width: double.infinity,
 
-            textAlign: TextAlign.center,
+          height: double.infinity,
 
-            style: TextStyle(
+          color: Colors.white,
 
-              color: Colors.white,
+          child:
+
+            Center(
+
+                child:
+
+                Column(
+
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: <Widget>[
+
+                    Container(
+
+                      width: 400,
+
+                      height: 600,
+                      
+                      padding: EdgeInsets.all(50),
+
+                      decoration: BoxDecoration(
+
+                        color: Colors.white,
+                        
+                        borderRadius: BorderRadius.circular(20),
+
+                        boxShadow: <BoxShadow>[
+
+                          BoxShadow(
+
+                            color: Colors.grey.withOpacity(0.8),
+
+                            spreadRadius: 5,
+
+                            blurRadius: 5,
+
+                            offset: const Offset(0, 3),
+
+                          )
+
+                        ],
+
+                      ),
+
+                      child: Center(
+
+                        child: Column(
+
+                          mainAxisAlignment: MainAxisAlignment.center,
+
+                          mainAxisSize: MainAxisSize.max,
+
+                          crossAxisAlignment: CrossAxisAlignment.center,
+
+                          children: <Widget>[
+
+                            TextField(
+
+                              decoration:
+
+                                InputDecoration(
+
+                                  labelText: "Usuario",
+                                  
+                                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
+
+                                ),
+
+                            ),
+
+                            SizedBox(height: 20),
+
+                            TextField(
+
+                              decoration:
+
+                              InputDecoration(
+
+                                  labelText: "Contraseña",
+
+                                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
+
+                              ),
+
+                            ),
+
+                          ],
+
+                        ),
+
+                      ),
+
+                    )
+
+                  ]
+
+                )
 
             ),
 
-          ),
+        )
 
-        ),
-
-        backgroundColor: Colors.red
-
-      ),
 
     );
 
