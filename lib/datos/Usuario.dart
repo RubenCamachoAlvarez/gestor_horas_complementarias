62a2a96a3c823a047de0cabd23ae4d39387726b2
RubenCA <ruben1000.ca@gmail.com>
2024-03-25 20:32:52 -0600

    Inclusion del mecanismo de logueo dentro de la aplicacion

---
+++ lib/datos/Usuario.dart
@@ -0,0 +1,24 @@
+
+import 'dart:ui';
+
+class Usuario {
+
+  /**
+   * Esta clase contiene campos/atributos estaticos que permiten al programa almacenar
+   * los datos del usuario una vez que este ha iniciado sesion satisfactoriamente.
+   */
+
+  static String? numero = null;
+
+  static String? nombre = null;
+
+  static String? apellidoPaterno = null;
+
+  static String? apellidoMaterno = null;
+
+  static DateTime? fechaNacimiento = null;
+
+  static Image? imagenPerfil = null;
+
+
+}
---
+++ lib/helpers/Sesion.dart
@@ -0,0 +1,35 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gestor_de_horas_complementarias/datos/Usuario.dart';
+import 'package:gestor_de_horas_complementarias/helpers/BaseDeDatos.dart';
+
+class Sesion {
+
+  static Future<bool> iniciarSesion(String numero, String password) async {
+
+      DocumentReference<Map<String, dynamic>>? documento = BaseDeDatos.conexion.collection("Usuarios").doc(numero);
+
+      DocumentSnapshot<Map<String, dynamic>>? datos = await documento.get();
+
+      if(datos != null && datos.exists) {
+
+        Map<String, dynamic> datosPersonales = datos.get("datos_personales");
+
+        Usuario.numero = numero;
+
+        Usuario.nombre = datosPersonales["nombre"];
+
+        Usuario.apellidoPaterno = datosPersonales["apellido_paterno"];
+
+        Usuario.apellidoMaterno = datosPersonales["apellido_materno"];
+
+        Usuario.fechaNacimiento = (datosPersonales["fecha_nacimiento"] as Timestamp).toDate();
+
+      }
+
+      return true;
+
+  }
+
+
+}
