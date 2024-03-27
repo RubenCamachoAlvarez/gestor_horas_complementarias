25ab3f2e01f49ea72c5bbdce41d186098e7d1a26
RubenCA <ruben1000.ca@gmail.com>
2024-03-27 15:13:59 -0600

    Se agrego una instancia hacia Firebase Cloud Storage

--- lib/helpers/BaseDeDatos.dart
+++ lib/helpers/BaseDeDatos.dart
@@ -1,8 +1,11 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class BaseDeDatos {
 
   static final conexion = FirebaseFirestore.instance;
+
+  static final almacenamiento = FirebaseStorage.instance;
 
 }
