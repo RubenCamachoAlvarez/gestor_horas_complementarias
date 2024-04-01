248fa612a790aa03fd73cf757ecbb18c9cefb70d
RubenCA <ruben1000.ca@gmail.com>
2024-03-31 18:20:50 -0600

    Cambios en el diseño

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gestor_de_horas_complementarias/App.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:gestor_de_horas_complementarias/helpers/Sesion.dart';
@@ -6,6 +7,14 @@ import 'firebase_options.dart';
 import 'dart:core';
 
 void main() async {
+
+  /*SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+
+    statusBarColor: Colors.transparent,
+
+    statusBarIconBrightness: Brightness.dark
+
+  ));*/
 
   //Inicializacion Firebase para utilizarla en esta aplicacion.
   await Firebase.initializeApp(
--- lib/vistas/InformacionUsuario.dart
+++ lib/vistas/InformacionUsuario.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gestor_de_horas_complementarias/datos/Usuario.dart';
 
@@ -18,32 +19,168 @@ class InformacionUsuarioState extends State<InformacionUsuarioWidget> {
   @override
   Widget build(BuildContext context) {
 
-    return ListView(
+    return Scaffold(
 
-      padding: EdgeInsets.all(0),
+      body: ListView(
 
-      children: [
+        padding: EdgeInsets.all(20),
 
-        /*
-        Image.asset("assets/Imagen_Usuario.png", width: 512, height: 512 ),
+        children: [
 
-        SizedBox(height: 20,),
+          Column(
 
-        Text("${Usuario.nombre} ${Usuario.apellidoPaterno} ${Usuario.apellidoMaterno}", textAlign: TextAlign.center, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,),),
+            mainAxisAlignment: MainAxisAlignment.center,
 
-        SizedBox(height: 20,),
+            crossAxisAlignment: CrossAxisAlignment.center,
 
-        Text("${Usuario.numero}", textAlign: TextAlign.center, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+            children: [ CircleAvatar(
 
-        SizedBox(height: 20,),
+                radius: 150,
 
-        Text("${Usuario.carrera!.id}", textAlign: TextAlign.center, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                backgroundImage: const AssetImage("assets/images/ImagenUsuario.png"),
 
-        SizedBox(height: 20,),
+                backgroundColor: Colors.white70.withOpacity(0.1),
 
-        Text("${Usuario.fechaNacimiento!.day}/${Usuario.fechaNacimiento!.month}/${Usuario.fechaNacimiento!.year}", textAlign: TextAlign.center, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-        */
-      ]
+              ),
+
+            ]
+
+          ),
+
+          const SizedBox(height: 10),
+
+          ListTile(
+
+            title: const Text("Nombre"),
+
+            subtitle: Text("${Usuario.nombre} ${Usuario.apellidoPaterno} ${Usuario.apellidoMaterno}"),
+
+            leading: const Icon(CupertinoIcons.person),
+
+            trailing: const Icon(Icons.arrow_drop_down, color: Colors.grey),
+
+            tileColor: Colors.grey[100],
+
+            shape: RoundedRectangleBorder(
+
+              borderRadius: BorderRadius.circular(10)
+
+            ),
+
+            titleTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+
+            horizontalTitleGap: 30,
+
+          ),
+
+          const SizedBox(height: 10,),
+
+          ListTile(
+
+            title: const Text("Fecha de nacimiento"),
+
+            subtitle: Text("${Usuario.fechaNacimiento!.day}/${Usuario.fechaNacimiento!.month}/${Usuario.fechaNacimiento!.year}"),
+
+            leading: const Icon(CupertinoIcons.calendar),
+
+            trailing: const Icon(Icons.arrow_drop_down, color: Colors.grey),
+
+            tileColor: Colors.grey[100],
+
+            shape: RoundedRectangleBorder(
+
+                borderRadius: BorderRadius.circular(10)
+
+            ),
+
+            titleTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+
+            horizontalTitleGap: 30,
+
+          ),
+
+          const SizedBox(height: 10,),
+
+          ListTile(
+
+            title: const Text("Carrera"),
+
+            subtitle: Text(Usuario.carrera!.id),
+
+            leading: const Icon(CupertinoIcons.book),
+
+            trailing: const Icon(Icons.arrow_drop_down, color: Colors.grey),
+
+            tileColor: Colors.grey[100],
+
+            shape: RoundedRectangleBorder(
+
+                borderRadius: BorderRadius.circular(10)
+
+            ),
+
+            titleTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+
+            horizontalTitleGap: 30,
+
+          ),
+
+          const SizedBox(height: 10,),
+
+          ListTile(
+
+            title: const Text("Ocupación"),
+
+            subtitle: Text(Usuario.rol!.id),
+
+            leading: const Icon(CupertinoIcons.alarm),
+
+            trailing: const Icon(Icons.arrow_drop_down, color: Colors.grey),
+
+            tileColor: Colors.grey[100],
+
+            shape: RoundedRectangleBorder(
+
+                borderRadius: BorderRadius.circular(10)
+
+            ),
+
+            titleTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+
+            horizontalTitleGap: 30,
+
+          ),
+
+          const SizedBox(height: 10,),
+
+          ListTile(
+
+            title: Text("Número de ${Usuario.rol!.id == "Encargado" ? "trabajador" : "cuenta"}"),
+
+            subtitle: Text(Usuario.numero!),
+
+            leading: const Icon(CupertinoIcons.creditcard),
+
+            trailing: const Icon(Icons.arrow_drop_down, color: Colors.grey),
+
+            tileColor: Colors.grey[100],
+
+            shape: RoundedRectangleBorder(
+
+                borderRadius: BorderRadius.circular(10)
+
+            ),
+
+            titleTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+
+            horizontalTitleGap: 30,
+
+          )
+
+        ],
+
+      )
+
 
     );
 
